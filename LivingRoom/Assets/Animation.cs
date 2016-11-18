fcd195a091d3ea52e897a10f9450e508f1e1d336
abdalmoneem <abdalmoneem.dev@gmail.com>
2016-11-18 21:38:00 +0200

    mini project 2

---
+++ LivingRoom/Assets/Animation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animation : MonoBehaviour {
+  
+    private Animator anim;
+    float v,h;
+
+
+
+	void Start () {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        v= Input.GetAxis("Vertical");
+        h = Input.GetAxis("Horizontal");
+    }
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        anim.SetFloat("walk", v);
+        anim.SetFloat("turn", h);
+        
+	}
+}
